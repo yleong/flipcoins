6acc2cda6cda3a4309dad5e50b3a495b285e2719
Yew Long <yewlong42@gmail.com>
2017-09-13 11:15:57 +0000

    generalize algorithm to n items instead of 3 items

--- flip.go
+++ flip.go
@@ -1,41 +1,53 @@
  /*
- Can we randomly choose 1 item out of 3 by flipping 2 coins to generate 2 bits
- and convert the bits to base 10 to get our item number. If we flipped a 4 then
- we will just discard and try again.  Question is, is this uniform?
+ Can we randomly choose 1 item out of n by flipping m = ceil(log_2(n)) coins to
+ generate m bits and convert the bits to base 10 to get our item number l. If l
+ > n  then we will just discard and try again.  Question is, is this uniform?
  */
-
 package main
 
-import ("fmt";"math/rand"; "time")
+import ("fmt";"math/rand"; "math"; "time")
 
 func flip() int {
     return rand.Intn(2)
 }
 
 /*
- The chance that any 1 of the 3 items getting chosen on first try is 1/4, on
- second try is 1/4*1/4, on third try is 1/4*1/4*1/4. So we will just sum this up
+ Given an integer n, returns the least number of flips needed to generate a
+ bitstring that covers at least until n. E.g., for n = 7, we have to flip 3
+ times for n = 30, we have to flip 5 times
+*/
+func numflips(set_size int) int {
+    log := math.Log2(float64(set_size))
+    return int(math.Pow(2, math.Ceil(log)))
+}
+
+/*
+ Define m to be the number of flips needed to cover n. The chance that any
+ 1 of the n items getting chosen on first try is 1/m, on second try is
+ (m-n)/m*1/m, on third try is (m-n)/m*(m-n)/m*1/m. So we will just sum this up
  Or we can probably use the geometric progression formula
 */
-func calculateUniform() {
-   sum := 0.25
+func calculateUniform(set_size int) {
+   totalItems := numflips(set_size)
+   multiplier := float64(totalItems - set_size)/float64(totalItems)
+   sum := 1.0/float64(totalItems)
    num_terms := 100
    for i, term := 0, sum; i < num_terms; i++ {
-        term *= 0.25 
+        term *= multiplier
         sum += term
    }
-   fmt.Printf("Probability of choosing 1 item out of 3: %f\n", sum)
+   fmt.Printf("Probability of choosing 1 item out of %d: %f\n", set_size, sum)
 }
 
 /*
  Alternatively we can just simulate the flipping for a large number of trials
- and check that we do get an even distribution in the end
+ and check that each item do get selected more or less with equal frequency in
+ the end
 */
-func trialUniform() {
+func trialUniform(set_size int) {
     num_trials := 10000
-    num_flips := 2
-    const set_size = 3
-    var hits [set_size]int
+    num_flips := numflips(set_size)
+    hits := make([]int, set_size)
     for i := 0; i < num_trials; i++ {
         currNum := 0
         currMultiplier := 1
@@ -43,7 +55,8 @@ func trialUniform() {
             currNum += flip() * currMultiplier
             currMultiplier *= 2
         }
-        if currNum == 3 {
+        if currNum >= set_size {
+            //discard this trial
             i--
             continue
         }
@@ -54,8 +67,18 @@ func trialUniform() {
     }
 }
 
+func testPowerOfTwo(){
+    for i := 0; i < 100; i++ {
+        fmt.Printf("the smallestpoweroftwo of %d is %d\n", i, numflips(i))
+    }
+}
+func testCalculateUniform(){
+    for i := 0; i < 100; i++ {
+        calculateUniform(i)
+    }
+}
 func main() {
     rand.Seed(time.Now().UTC().UnixNano())
-    calculateUniform()
-    trialUniform()
+    calculateUniform(3)
+    trialUniform(3)
 }
